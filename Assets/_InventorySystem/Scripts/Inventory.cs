64ee4fd6cad292462af14dcf51440ae8f13d5b90
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-09-20 14:58:30 +0200

    Added events to the Inventory to comply with the Observer Pattern

--- Assets/_InventorySystem/Scripts/Inventory.cs
+++ Assets/_InventorySystem/Scripts/Inventory.cs
@@ -1,12 +1,27 @@
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class Inventory
 {
+    public Action<IInventoryItem> OnItemAdded;
+    public Action<IInventoryItem> OnItemRemoved;
+
     private List<IInventoryItem> items = new();
 
-    public void AddItem(IInventoryItem item) => items.Add(item);
-    public void RemoveItem(IInventoryItem item) => items.Remove(item);
+    public void AddItem(IInventoryItem item)
+    {
+        items.Add(item);
+        OnItemAdded?.Invoke(item);
+    }
+
+    public void RemoveItem(IInventoryItem item)
+    {
+        if (items.Remove(item)) {
+            OnItemRemoved?.Invoke(item);
+        }
+    }
+
     public List<IInventoryItem> GetItems() => items;
 }
