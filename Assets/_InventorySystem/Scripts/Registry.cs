dcac6166bbf8742f24c80fc8d8c261d9dc9897ae
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-09-22 15:45:08 +0200

    Added a registry class to comply with the Service Locator pattern

---
+++ Assets/_InventorySystem/Scripts/Registry.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+// Service locator class to retreive and store objects/services
+public static class Registry
+{
+    private static Dictionary<string, object> objects = new();
+
+    // Adds an object if it does not already exist in the Registry
+    public static void Register(string key, object obj)
+    {
+        if(!objects.TryAdd(key, obj)) {
+            objects[key] = obj;
+        }
+    }
+
+    // Removes an object if it can be found in the Registry
+    public static bool Unregister(object obj)
+    {
+        if (!objects.ContainsValue(obj))
+            return false;
+
+        foreach (var item in objects.Keys) {
+            if (objects[item] == obj) {
+                objects[item] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Return an object with the given Registry Key
+    public static object Get(string key)
+    {
+        if (!objects.ContainsKey(key))
+            return null;
+        return objects[key];
+    }
+}
